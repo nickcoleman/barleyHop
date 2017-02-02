3c774317fee473b59d3ccc3b7e4332fe82d76688
Nick <nick@itprojmgt.com>
2017-02-02 13:30:22 -0700

    removed unused variable length

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,4 @@
 import { Actions } from 'react-native-router-flux'
-import axios from 'axios'
 import 'whatwg-fetch'
 
 
@@ -93,9 +92,9 @@ export const reverseGeoLocLookup = (lat = '40.732287', lon: '-111.8996689') => d
   const url2 = `https://maps.googleapis.com/maps/api/geocode/json?latlng=40.64177,-111.4946&key=${GOOGLE_API_KEY}`
   fetch(url)
     .then(response => response.json())
-    // .then(data => dispatch(fetchBreweryByGeoLocation(data)))
     .then(data => {
-      const location = data.results[0].address_components[3].long_name
+      const address = data.results[0].formatted_address.split(',')
+      const location = address[len-3]
       dispatch(fetchBreweryLocations(location))
     })
     .catch(error =>console.log('reverseGeoLocLookup Error: ', error))
